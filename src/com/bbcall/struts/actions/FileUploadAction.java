e249c1ee7338127b7f459a34403c2c87bd047ec9
dogdog107 <dogdog107@hotmail.com>
2015-06-26 14:09:23 +0800

    commit on 26Jun

---
+++ src/com/bbcall/struts/actions/FileUploadAction.java
@@ -0,0 +1,112 @@
+package com.bbcall.struts.actions;
+
+import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.struts2.ServletActionContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.bbcall.functions.ResultCode;
+import com.bbcall.struts.services.FileUploadServices;
+import com.opensymphony.xwork2.ActionSupport;
+
+@Scope("prototype")
+@Controller("fileUploadAction")
+@SuppressWarnings("serial")
+public class FileUploadAction extends ActionSupport {
+	
+	@Autowired
+	private FileUploadServices fileUploadServices;
+	
+	private Map<String, Object> dataMap = new LinkedHashMap<String, Object>(); // 新建dataMap来储存JSON字符串
+	private String savePath; // 封装保存文件的路径目录
+	private String title; // 封装上传文件的标题
+	private File upload; // 封装上传文件的内容
+	private Integer userid;
+	private String picurl;
+	
+	public String userUpload() throws Exception {
+		System.out.println("Here is FileUploadAction.userUpload()");
+		savePath = "/UserPhoto";
+		String storePath = ServletActionContext.getServletContext().getRealPath(savePath);
+		int result = fileUploadServices.uploadFile(upload, userid, storePath);
+		picurl = fileUploadServices.getFileurl();
+		
+		dataMap.clear(); // dataMap中的数据将会被Struts2转换成JSON字符串，所以这里要先清空其中的数据
+		dataMap.put("resultcode", result); // 放入一个是否操作成功的标识
+		dataMap.put("errmsg", ResultCode.getErrmsg(result));
+		dataMap.put("userUploadResult", true); // 放入registerResult
+		
+		return SUCCESS;
+	}
+	
+	public String userUploadJson() throws Exception {
+		System.out.println("Here is FileUploadAction.userUploadJson()");
+		userUpload();
+		return "json";
+	}
+	
+//	public String uploadFile() throws Exception {
+//
+//		// 保存文件的地址
+//		FileOutputStream fos = new FileOutputStream(getSavePath() + "\\"
+//				+ uploadFileName);
+//
+//		// 获得上传文件流
+//		FileInputStream fis = new FileInputStream(upload);
+//
+//		// 将文件写入服务器
+//		byte[] buffer = new byte[BUFFER_SIZE];
+//		int len = 0;
+//		while ((len = fis.read(buffer)) > 0) {
+//			fos.write(buffer, 0, len);
+//		}
+//    fos.close();
+//    fis.close();
+//		return SUCCESS;
+//	}
+
+	public String getSavePath() {
+		return ServletActionContext.getServletContext().getRealPath(savePath);
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public File getUpload() {
+		return upload;
+	}
+
+	public void setSavePath(String savePath) {
+		this.savePath = savePath;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setUpload(File upload) {
+		this.upload = upload;
+	}
+
+	public Integer getUserid() {
+		return userid;
+	}
+
+	public void setUserid(Integer userid) {
+		this.userid = userid;
+	}
+
+	public String getPicurl() {
+		return picurl;
+	}
+
+	public void setPicurl(String picurl) {
+		this.picurl = picurl;
+	}
+
+}
---
+++ src/com/bbcall/struts/services/FileUploadServices.java
@@ -0,0 +1,52 @@
+package com.bbcall.struts.services;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import org.springframework.stereotype.Service;
+
+import com.bbcall.functions.ResultCode;
+
+@Service("fileUploadServices")
+public class FileUploadServices{
+	private static final int BUFFER_SIZE = 16 * 1024;
+	private String fileurl;
+	
+	public int uploadFile(File srcFile, Integer userid, String storePath)
+			throws Exception {
+		String uploadFileName = srcFile.getName(); // 封装上传文件名
+		String uploadContentType = uploadFileName.substring(uploadFileName.lastIndexOf(".") + 1); // 封装上传文件类型
+		String storeFileName = userid + "_photo." + uploadContentType; // 封装保存文件名
+		File destFile = new File(storePath + "\\" + storeFileName);
+		
+		if(!destFile.exists()){
+			destFile.mkdirs();
+		} else {
+			destFile.delete();
+		}
+		
+		// 保存文件的地址
+		FileOutputStream fos = new FileOutputStream(destFile);
+
+		// 获得上传文件流
+		FileInputStream fis = new FileInputStream(srcFile);
+		
+		// 将文件写入服务器
+		byte[] buffer = new byte[BUFFER_SIZE];
+		int len = 0;
+		while ((len = fis.read(buffer)) > 0) {
+			fos.write(buffer, 0, len);
+		}
+		fos.close();
+		fis.close();
+		
+		fileurl = destFile.toURI().toURL().toString();
+		System.out.println(fileurl);
+		return ResultCode.SUCCESS;
+	}
+
+	public String getFileurl() {
+		return fileurl;
+	}
+}
