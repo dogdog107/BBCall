7da5484d8b68bc19120721a08e86728c73bf40a9
dogdog107 <dogdog107@hotmail.com>
2016-01-06 00:36:05 +0800

    Version update.

--- src/com/bbcall/functions/VersionControl.java
+++ src/com/bbcall/functions/VersionControl.java
@@ -3,7 +3,7 @@ package com.bbcall.functions;
 public class VersionControl {
 	private static String androidVer = "1.0.5";
 	private static String iosVer = "1.0.0";
-	private static String serverVer = "1.4.4";
+	private static String serverVer = "1.4.5";
 	
 	public static String getAndroidVer() {
 		return androidVer;
